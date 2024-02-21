61efc3a218c86af34e4d58a3a0719e304d513839
liuchenglong <768206581@qq.com>
2024-02-21 16:03:43 +0800

    更换 icon&设计页采用新框架

--- src/views/designer/index.vue
+++ src/views/designer/index.vue
@@ -1,23 +1,21 @@
 <template>
-
-  <a-row ref="el" style="margin:0 100px;">
-    <template v-for="item in list" :key="item.id" >
-      <a-col :span="2" v-if="item.type == 'icon' || item.type == 'add'"
-        class="flex flex-direction justify-center align-center"
-        style="height:100%;cursor: pointer; height: 90px;margin: 10px;">
+  <div ref="el" class="icon-scroll-content">
+    <template v-for="item in list">
+      <div :key="item.id" v-if="item.type == 'icon' || item.type == 'add'"
+        class="icon-box flex flex-direction justify-center align-center">
         <div style=" height: 70%;" :title='item.name'>
           <img class="app-item-img" :src="item.src" style=" height: 100%; border-radius: 15px;" />
         </div>
         <p style='flex:1;filter: drop-shadow(0px 2px 7px rgba(0,0,0,.1));margin-top: 5px;text-shadow: 0 0 2px #0000004d;'
           class="cl-ant-p sg-omit-sm text-white-sm">{{ item.name }}
         </p>
-      </a-col>
-      <a-col :span="4" v-else-if="item.type == 'component'" class="flex flex-direction justify-center align-center"
-        style="height:100%;cursor: pointer; height: 180px;margin: 10px;">
+      </div>
+      <div v-else-if="item.type == 'component'" :key="item.component"
+        class="comp-box flex flex-direction justify-center align-center">
         <HeadCalendar />
-      </a-col>
+      </div>
     </template>
-  </a-row>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -25,10 +23,11 @@ import { ref } from 'vue'
 import { useDraggable, type UseDraggableReturn } from 'vue-draggable-plus'
 import { useGridsStore } from '@/store/grids';
 import HeadCalendar from '../home/HeadCalendar.vue';
+const drag = ref(false)
 
 
 const el = ref()
-const list = ref()
+const list = ref([])
 onMounted(() => {
   console.log('()..navIconConfig', useGridsStore().getSelectedGrids.navIconConfig)
   list.value = useGridsStore().getSelectedGrids.navIconConfig
@@ -39,9 +38,70 @@ const draggable = useDraggable<UseDraggableReturn>(el, list, {
   animation: 150,
   onStart() {
     console.log('start')
+    drag.value = true
+  },
+  onEnd() {
+    console.log('onEnd')
+    nextTick(() => {
+      drag.value = false
+    })
   },
   onUpdate() {
     console.log('update')
   }
 })
+
+const sort = () => {
+  list.value.sort((a, b) => a.id - b.id)
+}
 </script>
+
+<style scoped lang="scss">
+.icon-scroll-content {
+  display: grid;
+  /* 这个属性用于定义网格布局中的列。在这个例子中，repeat(auto-fill, 200px)表示列的宽度为200像素，并且会自动填充容器的宽度，以适应容器的大小。 */
+  grid-template-columns: repeat(auto-fill, 100px);
+  /* 控制自动布局算法的流动方式。dense表示使用“密集”布局算法*/
+  grid-auto-flow: row dense;
+  /* 行列间距。 */
+  grid-gap: 10px;
+  justify-content: center;
+  grid-auto-rows: auto;
+  width: 80%;
+
+  .icon-box {
+    width: 100%;
+    height: 95px;
+    grid-column: span 1;
+    grid-row: span 1;
+    cursor: pointer;
+    padding: 10px 0px;
+  }
+
+  .comp-box {
+    grid-column: span 2;
+    grid-row: span 2;
+    grid-column-start: span 2;
+    grid-row-start: span 2;
+    height: 200px;
+  }
+}
+</style>
+
+<style>
+.fade-move,
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: scaleY(0.01) translate(30px, 0);
+}
+
+.fade-leave-active {
+  position: absolute;
+}
+</style>
